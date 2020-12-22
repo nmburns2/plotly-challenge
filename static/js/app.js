633e82606e4c7ce3ccb35ae908c655004b95f2cf
Nicholas Burns <nmburns@triad.rr.com>
2020-12-21 19:16:08 -0500

    edits

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,22 @@
-
+// Create arrays to hold data
 var id = [];
 var meta = [];
 var samples = [];
 
 
 function init() {
-
     d3.json("data/samples.json").then(function (data) {
-
-        
         id = data.names;
         meta = data.metadata;
         samples = data.samples;
 
-        
-        var selection = d3.select("#selDataset");
+        var select = d3.select("#selDataset");
         id.forEach(element => {
-            var options = selection.append("option");
+            var options = select.append("option");
             options.property("value", element);
             options.text(element);
         });
-
-        
-        optionChanged(selection.property("value"));
+            optionChanged(selection.property("value"));
     });
 }
 
@@ -30,28 +24,26 @@ function init() {
 init();
 
 
-
+// Create function to call graphs
 function optionChanged(id) {
     barPlot(id);
     bubbleChart(id);
     demoGraphic(id);
 };
 
-
+// Create function for horizontal bar graph
 function barPlot(id) {
 
-
+    // Filter Data 
     var filtered = samples.filter(event => parseInt(event.id) === parseInt(id));
-
-  
     var samp_data = filtered[0];
 
-   
+    // Create sample values, ids, and labels
     var sample_values = samp_data.sample_values.slice(0, 10).reverse();
     var otu_ids = samp_data.otu_ids.slice(0, 10).reverse();
     var otu_labels = samp_data.otu_labels.slice(0, 10).reverse();
 
-    
+    // Create trace
     var trace = {
         x: sample_values,
         y: otu_ids.map(id => `OTU ${id}`),
@@ -60,10 +52,10 @@ function barPlot(id) {
         orientation: "h",
     };
 
-
+    // Edit Data
     var data = [trace];
 
- 
+    // Edit layout
     var layout = {
         title: `<b>Top OTUs Found`,
         yaxis: {
@@ -74,24 +66,23 @@ function barPlot(id) {
         }
     };
 
- 
+    // Create bar chart
     Plotly.newPlot("bar", data, layout);
 };
 
+// Create function for bubble chart
 function bubbleChart(id) {
 
-    
+    // Filter data
     var filtered = samples.filter(event => parseInt(event.id) === parseInt(id));
-
-    
     var samp_data = filtered[0];
 
-    
+    // create sample values, ids, and labels
     var sample_values = samp_data.sample_values;
     var otu_ids = samp_data.otu_ids;
     var otu_labels = samp_data.otu_labels;
 
-
+    // Create trace
     var trace = {
         x: otu_ids,
         y: sample_values,
@@ -104,9 +95,10 @@ function bubbleChart(id) {
         color: sample_values,
     };
 
+    // Edit Data 
     var data = [trace];
 
-    
+    // Edit layout
     var layout = {
         title: `<b>OTUs Found`,
         showlegend: false,
@@ -118,6 +110,7 @@ function bubbleChart(id) {
         }
     };
   
+    // Create bubble chart
     Plotly.newPlot('bubble', data, layout);
 }
 
